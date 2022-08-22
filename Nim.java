2aa13c6dbc1bf91e61ed3773111cf48f01b715da
Sreya Nandanampati <111778548+sreyanan03@users.noreply.github.com>
2022-08-22 13:46:45 -0400

    Create Nim.java

---
+++ Nim.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Nim{
+    public static void main(String[] args){
+        
+        System.out.println("Welcome to Nim death match!");
+        
+        Scanner input = new Scanner(System.in);
+        System.out.println("What difficulty level do you want to play to: 1/2?");
+        int level = input.nextInt();
+        Game g = new Game(level);
+        g.play();
+        
+        System.out.println("Thanks for playing!");
+    }
+}
